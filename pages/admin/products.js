ed0b93f8997d3b1a73ae9b50aeae63291451aed4
Moinulilm10 <moinulilm10@gmail.com>
2023-04-12 21:51:01 +0600

    dashboard/users added

--- pages/admin/products.js
+++ pages/admin/products.js
@@ -3,6 +3,8 @@ import { getError } from "@/utils/error";
 import axios from "axios";
 import Link from "next/link";
 import React, { useEffect, useReducer } from "react";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 
 function reducer(state, action) {
   switch (action.type) {
@@ -12,16 +14,50 @@ function reducer(state, action) {
       return { ...state, loading: false, products: action.payload, error: "" };
     case "FETCH_FAIL":
       return { ...state, loading: false, error: action.payload };
+    case "CREATE_REQUEST":
+      return { ...state, loadingCreate: true };
+    case "CREATE_SUCCESS":
+      return { ...state, loadingCreate: false };
+    case "CREATE_FAIL":
+      return { ...state, loadingCreate: false };
+    case "DELETE_REQUEST":
+      return { ...state, loadingDelete: true };
+    case "DELETE_SUCCESS":
+      return { ...state, loadingDelete: false, successDelete: true };
+    case "DELETE_FAIL":
+      return { ...state, loadingDelete: false };
+    case "DELETE_RESET":
+      return { ...state, loadingDelete: false, successDelete: false };
     default:
       state;
   }
 }
 export default function AdminProdcutsScreen() {
-  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+  const router = useRouter();
+  const [
+    { loading, error, products, loadingCreate, successDelete, loadingDelete },
+    dispatch,
+  ] = useReducer(reducer, {
     loading: true,
     products: [],
     error: "",
   });
+
+  const createHandler = async () => {
+    if (!window.confirm("Are you sure?")) {
+      return;
+    }
+    try {
+      dispatch({ type: "CREATE_REQUEST" });
+      const { data } = await axios.post(`/api/admin/products`);
+      dispatch({ type: "CREATE_SUCCESS" });
+      toast.success("Product created successfully");
+      router.push(`/admin/product/${data.product._id}`);
+    } catch (err) {
+      dispatch({ type: "CREATE_FAIL" });
+      toast.error(getError(err));
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -34,8 +70,28 @@ export default function AdminProdcutsScreen() {
       }
     };
 
-    fetchData();
-  }, []);
+    if (successDelete) {
+      dispatch({ type: "DELETE_RESET" });
+    } else {
+      fetchData();
+    }
+  }, [successDelete]);
+
+  const deleteHandler = async (productId) => {
+    if (!window.confirm("Are you sure?")) {
+      return;
+    }
+    try {
+      dispatch({ type: "DELETE_REQUEST" });
+      await axios.delete(`/api/admin/products/${productId}`);
+      dispatch({ type: "DELETE_SUCCESS" });
+      toast.success("Product deleted successfully");
+    } catch (err) {
+      dispatch({ type: "DELETE_FAIL" });
+      toast.error(getError(err));
+    }
+  };
+
   return (
     <Layout title="Admin Products">
       <div className="grid md:grid-cols-4 md:gap-5">
@@ -52,8 +108,8 @@ export default function AdminProdcutsScreen() {
               </Link>
             </li>
             <li>
-              <Link legacyBehavior href="/admin/products">
-                <a className="font-bold">Products</a>
+              <Link legacyBehavior href="/admin/products" className="font-bold">
+                Products
               </Link>
             </li>
             <li>
@@ -64,7 +120,17 @@ export default function AdminProdcutsScreen() {
           </ul>
         </div>
         <div className="overflow-x-auto md:col-span-3">
-          <h1 className="mb-4 text-xl">Products</h1>
+          <div className="flex justify-between">
+            <h1 className="mb-4 text-xl">Products</h1>
+            {loadingDelete && <div>Deleting item...</div>}
+            <button
+              disabled={loadingCreate}
+              onClick={createHandler}
+              className="primary-button"
+            >
+              {loadingCreate ? "Loading" : "Create"}
+            </button>
+          </div>
           {loading ? (
             <div>Loading...</div>
           ) : error ? (
@@ -96,11 +162,19 @@ export default function AdminProdcutsScreen() {
                         <Link
                           legacyBehavior
                           href={`/admin/product/${product._id}`}
+                          type="button"
+                          className="default-button"
                         >
                           Edit
                         </Link>
                         &nbsp;
-                        <button>Delete</button>
+                        <button
+                          onClick={() => deleteHandler(product._id)}
+                          className="default-button"
+                          type="button"
+                        >
+                          Delete
+                        </button>
                       </td>
                     </tr>
                   ))}
--- pages/api/admin/products/[id]/index.js
+++ pages/api/admin/products/[id]/index.js
@@ -1,18 +1,19 @@
 import Product from "@/models/Product";
 import db from "@/utils/db";
-import { getSession } from "next-auth/react";
+import { getToken } from "next-auth/jwt";
 
 const handler = async (req, res) => {
-  const session = await getSession({ req });
-  if (!session || (session && !session.user.isAdmin)) {
+  const user = await getToken({ req });
+  if (!user || (user && !user.isAdmin)) {
     return res.status(401).send("signin required");
   }
 
-  const { user } = session;
   if (req.method === "GET") {
     return getHandler(req, res, user);
   } else if (req.method === "PUT") {
     return putHandler(req, res, user);
+  } else if (req.method === "DELETE") {
+    return deleteHandler(req, res, user);
   } else {
     return res.status(400).send({ message: "Method not allowed" });
   }
@@ -38,6 +39,18 @@ const putHandler = async (req, res) => {
     await product.save();
     await db.disconnect();
     res.send({ message: "Product updated successfully" });
+  } else {
+    await db.disconnect();
+    res.status(404).send({ message: "Product not found" });
+  }
+};
+const deleteHandler = async (req, res) => {
+  await db.connect();
+  const product = await Product.findById(req.query.id);
+  if (product) {
+    await product.remove();
+    await db.disconnect();
+    res.send({ message: "Product deleted successfully" });
   } else {
     await db.disconnect();
     res.status(404).send({ message: "Product not found" });
